18928837c3c45e7a01790195049253cc3c631ac4
amn_s <Amnzera160@>
2023-07-22 12:53:23 -0300

    fix node version runtime

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -40,6 +40,7 @@ export class ProductsAppStack extends cdk.Stack {
             functionName: "ProductsFetchFunction",
             entry: "lambda/products/productsFetchFunction.ts",
             handler: "handler",
+            runtime: lambda.Runtime.NODEJS_16_X,
             memorySize: 128,
             timeout: cdk.Duration.seconds(5),
             bundling: {
@@ -58,6 +59,7 @@ export class ProductsAppStack extends cdk.Stack {
             functionName: "ProductsAdminFunction",
             entry: "lambda/products/productsAdminFunction.ts",
             handler: "handler",
+            runtime: lambda.Runtime.NODEJS_16_X,
             memorySize: 128,
             timeout: cdk.Duration.seconds(5),
             bundling: {
